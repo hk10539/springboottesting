d934a3b53a2cc4a2489b759515e31f2cccedd5d7
Harsh Kumar <hk10539@gmail.com>
2025-12-27 19:56:57 +0530

    27 Dec after class code

--- src/main/java/com/project1/springboottesting/SpringboottestingApplication.java
+++ src/main/java/com/project1/springboottesting/SpringboottestingApplication.java
@@ -2,12 +2,19 @@ package com.project1.springboottesting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringboottestingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboottestingApplication.class, args);
+	}
+	@GetMapping("/")
+	public String homepage(){
+		return "Homepage";
 	}
 
 }
